f8be935682de80b2a755be926a92ee5e157889cc
Timothée Peignier <timothee.peignier@tryphon.org>
2011-12-02 16:46:15 +0100

    improving and adding new tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,4 @@
 import os
-import os
 local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 
 DATABASES = {
@@ -12,6 +11,7 @@ DATABASES = {
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'django.contrib.auth',
     'django.contrib.admin',
     'pipeline',
@@ -22,10 +22,43 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = local_path('media')
 
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
+STATIC_ROOT = local_path('static/')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    local_path('assets/'),
+)
+STATICFILES_FINDERS = (
+    'pipeline.finders.PipelineFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder'
+)
 
-STATIC_ROOT = local_path('assets')
+PIPELINE_ROOT = local_path('assets/')
+PIPELINE_URL = STATIC_URL
+PIPELINE_TEMPLATE_EXT = ".jst"
 
 TEMPLATE_DIRS = (
     local_path('templates'),
 )
+
+PIPELINE_CSS = {
+    'screen': {
+        'source_filenames': (
+            'css/first.css',
+            'css/second.css',
+            'css/urls.css',
+        ),
+        'output_filename': 'screen.css'
+    }
+}
+PIPELINE_JS = {
+    'scripts': {
+        'source_filenames': (
+            'js/first.js',
+            'js/second.js',
+            'js/application.js',
+            'templates/**/*.jst'
+        ),
+        'output_filename': 'scripts.css'
+    }
+}
